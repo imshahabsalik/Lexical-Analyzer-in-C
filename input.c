6e1e072fe6f0f034590c73e15db99cccce002951
Shahab Salik <116941803+imshahabsalik@users.noreply.github.com>
2026-04-28 20:41:55 +0530

    Refactor main function with additional variables and comments
    Refactor main function to include various data types and operations, and add comments.

--- input.c
+++ input.c
@@ -1,16 +1,44 @@
+#include <stdio.h>
+#define MAX 100
+
 int main() {
-    int a;
-    int b;
-    int result;
+    int a = 10;
+    float b = 3.14;
+    char c = 'x';
 
-    a = 10;
-    b = 20;
+    double d = 1.2e10;
+    int hex = 0x1A;
+    int oct = 077;
 
-    result = a + b;
+    char newline = '\n';
+    char tab = '\t';
 
-    if (a < b) {
-        result = result + 5;
+    printf("Hello World\n");
+    printf("Value: %d\n", a);
+
+    if (a == 10 && b >= 3.0) {
+        a++;
+    } else {
+        a--;
     }
 
-    return result;
+    a += 5;
+    b *= 2;
+    a = a << 2;
+    a = a >> 1;
+
+    // This is a single-line comment
+
+    /*
+       This is a multi-line comment
+       spanning multiple lines
+    */
+
+    int invalid1 = 123abc;
+    char invalid2 = 'ab;
+    "unclosed string
+
+    @ $ #
+
+    return 0;
 }
